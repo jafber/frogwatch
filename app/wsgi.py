d64738c0c8883648fc3c5c42ffc0a4958943d596
Jan Berndt <JBerndt@netz.pbit-systeme.de>
2023-01-01 17:09:08 +0100

    a

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,9 +1,19 @@
-from flask import Flask, Response
+from flask import Flask, request
+
+import secret
 from camera import Camera
 from servo import Servo
 
 app = Flask(__name__)
 servo = None
+
+@app.route('/secret/gist_url')
+def gist_ip():
+	try:
+		assert request.json['password'] == secret.password
+		return secret.gist_url
+	except:
+		return 404, ''
 
 @app.route('/servo_left', methods=['POST'])
 def servo_left():

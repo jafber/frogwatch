68d1e785ac603869ef9a479d37bac42284092f96
CheeseCrustery <spam@jan-berndt.de>
2023-10-18 00:19:22 +0200

    frontend rework complete (untested)

--- front/index.js
+++ front/index.js
@@ -1,146 +1,143 @@
-const backendUrl = 'wss://jan-berndt.de/frogcam/ws/front'
-const wsTimeoutS = 10
-const imageSensorWidth = 3280
-const imageSensorHeight = 2464
-const messageType = {
+const STREAM_URL = 'wss://jan-berndt.de/frogcam/ws/front'
+const WS_TIMEOUT_S = 5
+const VIDEO_WIDTH = 1640
+const VIDEO_HEIGHT = 1232
+const MESSAGE_TYPE = {
     open: 'auth',
     getImage: 'get_image',
 }
 
-let canvas = null
-let playButton = null
-let lastRefresh = new Date()
-let socket = null
-let checkTask = null
-let currentImage = new Blob()
-
-// get an XOR of all the bytes
-async function blobHash() {
+// get a byte representing the XOR of all the blob's bytes
+async function blobHash(imageBlob) {
     let hash = 0
-    let binArray = await currentImage.arrayBuffer()
-    let view = new DataView(binArray)
+    if (!imageBlob) return hash
+    const binArray = await imageBlob.arrayBuffer()
+    const view = new DataView(binArray)
     for (let i = 0; i < view.byteLength; i++) {
         hash ^= view.getUint8(i)
     }
     return hash
 }
 
-// use socket to send a message
-async function send(msgType) {
+// use websocket to send a message
+async function webSocketMessage(webSocket, msgType, currentImageBlob = null) {
     let msg = {
         type: msgType,
     }
     switch(msgType) {
-        case (messageType.open):
+        case (MESSAGE_TYPE.open):
             msg['session'] = crypto.randomUUID()
             break
-        case (messageType.getImage):
-            msg['current_hash'] = await blobHash()
+        case (MESSAGE_TYPE.getImage):
+            msg['current_hash'] = await blobHash(currentImageBlob)
             break
         default:
             throw 'wrong message type'
     }
-    await socket.send(JSON.stringify(msg))
+    await webSocket.send(JSON.stringify(msg))
 }
 
-// draw a jpeg blob onto the canvas
-async function drawBlob() {
-    const bitmap = await createImageBitmap(currentImage)
+// draw a landscape 1640x1232 jpeg blob onto the canvas
+async function drawBlob(image, canvas) {
+    const bitmap = await createImageBitmap(image)
     const context = canvas.getContext('2d')
     context.drawImage(bitmap,
-        0, // dx
-        0, // dy
-        bitmap.width, // dWidth
-        bitmap.height // dHeight
-    )
+                      0, // dx
+                      0, // dy
+                      bitmap.width, // dWidth
+                      bitmap.height // dHeight
+                      )
 }
 
-// close old websocket and clear image
-function cleanUp() {
-    if (socket) socket.close()
-    if (checkTask) clearInterval(checkTask)
-    playButton.hidden = false
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-}
-
-// create a new websocket
-function setupWebsocket() {
-
-    // close old socket
-    cleanUp()
-
-    // start websocket ping-pong
-    socket = new WebSocket(backendUrl)
-    playButton.hidden = true
-    socket.addEventListener('open', function () {
-        send(messageType.open)
-        send(messageType.getImage)
-    })
- 
-    // refresh canvas image with each incoming message
-    lastRefresh = new Date()
-    socket.addEventListener('message', async function (event) {
-        lastRefresh = new Date()
-        currentImage = event.data
-        await drawBlob()
-        send(messageType.getImage)
-    })
-
-    // check if connection is still alive
-    checkTask = setInterval(() => {
-        if (new Date() - lastRefresh >= wsTimeoutS * 1000) cleanUp()
-    }, wsTimeoutS * 1000)
-}
-
-// transform the canvas so that the backends images of size imageSensorWidth*imageSensorHeight are displayed correctly
-function resizeCanvas() {
-    canvas.width = imageSensorWidth/2
-    canvas.height = imageSensorHeight/2
-    // calculate h and w so canvas covers the container
-    const ratio = imageSensorWidth / imageSensorHeight
-    let pw = canvas.parentElement.offsetWidth
-    let ph = canvas.parentElement.offsetHeight
-    if (ph / pw >= ratio) {
-        // container is too high
-        var h = ph
-        var w = ph / ratio
+// adjust canvas size and overflow direction depending on device aspect ratio
+function setCanvasBehavior(canvas) {
+    const container = canvas.parentElement
+    const videoWidthRatio = VIDEO_WIDTH / VIDEO_HEIGHT
+    const containerWidthRatio = container.offsetWidth / container.offsetHeight
+    if (containerWidthRatio > videoWidthRatio) {
+        // container is wider than video, scroll video top to bottom
+        canvas.style.height = 'initial'
+        canvas.style.width = '100%'
+        container.style.overflowX = 'clip'
+        container.style.overflowY = 'scroll'
     } else {
-        // container is too wide
-        var w = pw
-        var h = pw * ratio
+        // container is narrower than video, scroll video left to right
+        canvas.style.height = '100%'
+        canvas.style.width = 'initial'
+        container.style.overflowX = 'scroll'
+        container.style.overflowY = 'clip'
     }
-
-    // set canvas style
-    canvas.style.transform = 'rotate(-90deg)'
-    canvas.style.width = h + 'px'
-    canvas.style.height = w + 'px'
 }
 
 // open dialog to save the current image
-function saveImage() {
+function saveImage(imageBlob) {
     // https://stackoverflow.com/a/25911218/10666216
-    var blobUrl = URL.createObjectURL(currentImage);
-    var link = document.createElement("a")
+    if (!imageBlob) return
+    const blobUrl = URL.createObjectURL(imageBlob);
+    let link = document.createElement("a")
     link.href = blobUrl
     link.download = `frosch-${new Date().toISOString()}.jpg`
     link.click()
 }
 
-function turnServo(toRight) {
+// init and maintain websocket connection to the backend image stream
+function setupWebsocket(onMessage, onClose) {
+    // tell backend to start sending
+    const webSocket = new WebSocket(STREAM_URL)
+    webSocket.addEventListener('open', async function () {
+        await webSocketMessage(webSocket, MESSAGE_TYPE.open)
+        await webSocketMessage(webSocket, MESSAGE_TYPE.getImage)
+    })
 
+    // listen for incoming messages
+    let lastRefresh = new Date()
+    webSocket.addEventListener('message', async function (event) {
+        lastRefresh = new Date()
+        await onMessage(event.data)
+        await webSocketMessage(webSocket, MESSAGE_TYPE.getImage, event.data)
+    })
+
+    // check if stream is still alive
+    const timeout = WS_TIMEOUT_S * 1000
+    const checkTask = setInterval(() => {
+        if (new Date() - lastRefresh >= timeout) {
+            webSocket.close()
+            clearInterval(checkTask)
+        }
+    }, timeout)
+    webSocket.addEventListener('close', onClose)
 }
 
-window.addEventListener('load', () => {
-    canvas = document.getElementById('output')
-    playButton = document.getElementById('playbutton')
-    resizeCanvas()
-    setupWebsocket()
+// when site loads or play button is pressed, attempt to start the video
+function startVideo(playButton, canvas) {
+    playButton.hidden = true
+    const handleClose = () => {
+        playButton.hidden = false
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    }
+    const output = {}
+    setupWebsocket(async (imageBlob) => {
+        output.currentImage = imageBlob
+        await drawBlob(imageBlob, canvas)
+    }, handleClose)
+    return output
+}
 
-    // register service worker for pwa
-    // console.log('checking service worker', 'serviceWorker' in navigator)
-    // if ('serviceWorker' in navigator) {
-    //     navigator.serviceWorker.register('/sw.js')
-    //     console.log('service worker registered')
-    // }
+// main
+window.addEventListener('load', async () => {
+    // init canvas
+    const canvas = document.getElementById('output')
+    setCanvasBehavior(canvas)
+    window.addEventListener('resize', () => setCanvasBehavior(canvas))
+
+    // start the websocket stream
+    const playButton = document.getElementById('playbutton')
+    let video = startVideo(playButton, canvas)
+    playButton.addEventListener('click', () => {
+        video = startVideo(playButton, canvas)
+    })
+
+    // hydrate photo button
+    const photoButton = document.getElementById('photobutton')
+    photoButton.addEventListener('click', () => saveImage(video.currentImage))
 })
-window.addEventListener('resize', resizeCanvas)

bdab1f3b15b17a23d9a090838a27d72216d5a146
jafber <spam@jan-berndt.de>
2024-09-08 20:17:36 +0200

    modified raspberry ip script

--- get-raspberry-ip.py
+++ get-raspberry-ip.py
@@ -2,17 +2,25 @@
 
 from requests import get
 
-ip = get('https://gist.githubusercontent.com/CheeseCrustery/a80945ec5a6d0dfa8e067b0f9849d71c/raw/ipv4.txt').text.strip()
+# DEPRECATED, USE SSHPI INSTEAD
+# make 'raspberry' dns entry in /etc/hosts
+# ssh pi@raspberry
+ip = get('https://gist.githubusercontent.com/jafber/a80945ec5a6d0dfa8e067b0f9849d71c/raw/ipv4.txt').text.strip()
 print(ip)
 hosts = '/etc/hosts'
 try:
     with open(hosts, 'r') as f:
         lines = f.readlines()
+    wrote = False
     for i in range(len(lines)):
         if 'raspberry' in lines[i]:
+            print('writing ip to /etc/hosts')
             lines[i] = f'{ip}\traspberry\n'
             break
+    if not wrote:
+        print('writing ip to /etc/hosts')
+        lines.append(f'{ip}\traspberry')
     with open(hosts, 'w') as f:
         f.writelines(lines)
-except:
-    pass
+except Exception as e:
+    print(e)
